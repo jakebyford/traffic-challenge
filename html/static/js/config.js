8537afa9f7d04415e20731040f1e838df417af0c
Jake Byford <jake_byford@outlook.com>
2020-10-25 21:46:02 -0400

    add group project into my own single repository

---
+++ html/static/js/config.js
@@ -0,0 +1,4 @@
+const API_KEY = "pk.eyJ1IjoiamFrZWJ5Zm9yZCIsImEiOiJja2dwdXkwOHowb2lzMnNtdGY0MGgwZmFzIn0.B5RwmmUTVSmxpwl6mP82wA";
+const BASE_URL = 'https://phantomsoul-traffic-challenge.herokuapp.com/';
+//const BASE_URL = 'http://127.0.0.1:5000/';
+
